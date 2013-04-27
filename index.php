ca68e9f77e406e4d48493de7ec26b9b0a4811160
Alvin Grant <alving.nyc@gmail.com>
2013-04-27 17:27:45 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: FourSquare Local Explorer
+Description:  Shows recent checkin activity around certain geographic areas. Useful for neighborhood guides/hyperlocal blogs.
+Author: Jeff Marx
+*/
+
+
+
+?>
