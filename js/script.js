ad290655d8149e9b697b89ed1489a0ee88e836f8
hanca013 <hanca013@newschool.edu>
2013-04-28 12:52:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-jQuery(document).ready(function($){}
+// Initialize the JavaScript SDK
+WL.init({ 
+    client_id: '000000004C0E2C11', 
+    redirect_uri: skydrive.jeffreymarx.net,
+});
+
+$(document).ready(function() {
+   $getJson()
+     
+ });
+
+});
